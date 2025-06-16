4efe197c20e6634696cbf4888de60fbfed1a2269
menes-ux <m.adisso@alustudent.com>
2025-06-16 12:36:47 +0200

    Adding 8 more students, and creating the startup.sh file. From that we can already test the reminder file.

--- create_environment.sh
+++ create_environment.sh
@@ -22,15 +22,29 @@ cp functions.sh submission_reminder_$NAME/modules/
 cp submissions.txt submission_reminder_$NAME/assets
 cp config.env submission_reminder_$NAME/config
 
-#Here I am appending 5 more students records to submissions.txt so that we can test the application better! 
+#Here I am appending 10+ more students records to submissions.txt so that we can test the application better! 
 echo "Menes, Git, submitted" >> submission_reminder_$NAME/assets/submissions.txt
 echo "Aseye, Shell Basics, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
 echo "David, Shell Navigation, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
 echo "Annick, Git, submitted" >> submission_reminder_$NAME/assets/submissions.txt
 echo "Teejay, Shell Basics, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
-
-#
-...
+echo "Grace, Emacs, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Chisom, vi, submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Annet, vi, submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Bekey, Git, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Alvin, Shell Navigation, submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Theodora, Shell Basics, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Franck, Shell Navigation, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
+echo "Ayobamidele, Git, not submitted" >> submission_reminder_$NAME/assets/submissions.txt
+#now we are implementing the startup.script. This script just starts up the reminder app when executed
+cat <<EOF > submission_reminder_$NAME/startup.sh
+#!/bin/bash
+#We are using "source" for config.env and functions.sh becouse it reads and executes the content of a file in the current shell session.
+source ./config/config.env
+source ./modules/functions.sh
+#And here we use "bash" since we want it to run in a new subshell.
+bash ./app/reminder.sh
+EOF
 
 #To make all the files executable, we are using this command:
 find submission_reminder_$NAME -type f -name "*.sh" -exec chmod +x {} \;
