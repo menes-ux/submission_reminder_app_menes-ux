b4a6d152fde5978f49bb5802595a9afabb68560b
menes-ux <m.adisso@alustudent.com>
2025-06-17 12:00:40 +0200

    New branch

--- submission_reminder_David/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#We are using "source" for config.env and functions.sh becouse it reads and executes the content of a file in the current shell session.
-source ./config/config.env
-source ./modules/functions.sh
-#And here we use "bash" since we want it to run in a new subshell.
-bash ./app/reminder.sh
